8bf72b07b6426665f7c9e7ae3456960810bb5230
Valentin Marmin <valentin.marmin@gmail.com>
2020-09-15 15:16:43 -0400

    (projects) #3: creating basic projects app

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello_world',
+    'projects',
 ]
 
 MIDDLEWARE = [
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,4 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('projects/', include("projects.urls"))
 ]
