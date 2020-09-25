c143f95b79ce1f03ad4900b0ec4a69f49afa26b9
Valentin Marmin <valentin.marmin@gmail.com>
2020-09-24 22:13:49 -0400

    (#4): updating blog app with post queries

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from blog.models import Post
 
 def index(request):
-    return render(request, 'index.html', {})
+    featured = Post.objects.filter(featured=True)
+    latest = Post.objects.order_by('-timestamp')[0:3]
+    context = {
+        'object_list': featured,
+        'latest': latest
+    }
+    return render(request, 'index.html', context)
