0fcbd80df49d709993d395ccefd38f6ecbb2e5a7
Valentin Marmin <valentin.marmin@gmail.com>
2020-09-24 22:39:22 -0400

    (#4): pursuing template integration

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from blog.models import Post
+from marketing.models import Signup
+
 
 def index(request):
     featured = Post.objects.filter(featured=True)
-    latest = Post.objects.order_by('-timestamp')[0:3]
-    context = {
-        'object_list': featured,
-        'latest': latest
-    }
-    return render(request, 'index.html', context)
+    latest = Post.objects.order_by("-timestamp")[0:3]
+    if request.method == 'POST':
+        email = request.POST['email']
+        new_signup = Signup()
+        new_signup.email = email
+        new_signup.save()
+
+    context = {"object_list": featured, "latest": latest}
+
+    return render(request, "index.html", context)
